5cc2ab98ae4db6a2cd35eac2b98b62bd6e1d1b4c
Alex <amaklov2002@gmail.com>
2021-12-13 21:40:04 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const Telegraf = require('telegraf');
+const api = require('covid19-api');
+const Markup = require('telegraf/markup');
+const COUNTRIES_LIST = require('./constants');
+const BOT_TOKEN = require('./token');
+
+const bot = new Telegraf(BOT_TOKEN);
+bot.start((ctx) =>
+  ctx.reply(
+    `
+Привет ${ctx.message.from.first_name}!
+Узнай статистику по Коронавирусу.
+Введи на английском название страны и получи статистику.
+Посмотреть весь список стран можно командой /help.\nСоздатель бота @SneakyThrowss
+`,
+    Markup.keyboard([
+      ['US', 'Russia'],
+      ['Ukraine', 'Kazakhstan'],
+    ])
+      .resize()
+      .extra()
+  )
+);
+
+bot.help((ctx) => ctx.reply(COUNTRIES_LIST));
+
+bot.on('text', async (ctx) => {
+  let data = {};
+
+  try {
+    data = await api.getReportsByCountries(ctx.message.text);
+
+    const formatData = `
+Страна: ${data[0][0].country}
+Случаи: ${data[0][0].cases}
+Смертей: ${data[0][0].deaths}
+Вылечились: ${data[0][0].recovered}
+  `;
+    ctx.reply(formatData);
+  } catch {
+    ctx.reply('Ошибка, такой страны не существует, посмотрите /help.');
+  }
+});
+bot.launch();
+
+
+console.log('Бот запущен');
